10bbc0f6ccdbd0e70b0494c542932c560a1be38b
ben <kipbena23@gmail.com>
2024-01-13 17:02:29 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
+    id ("dagger.hilt.android.plugin")
+}
+val navigationVersion =rootProject.extra["navigationVersion"]
+val hiltVersion = rootProject.extra["hiltVersion"]
+val hiltAndroidXVersion = rootProject.extra["hiltAndroidXVersion"]
+
+android {
+    namespace = "com.example.digitalrecoder"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.digitalrecoder"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //making it fit for all screens
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+
+    //splash screen
+    implementation("androidx.core:core-splashscreen:1.1.0-alpha02")
+
+    // Dagger Hilt
+    implementation( "com.google.dagger:hilt-android:$hiltVersion")
+    kapt ("com.google.dagger:hilt-android-compiler:$hiltVersion")
+    kapt( "androidx.hilt:hilt-compiler:$hiltAndroidXVersion")
+
+    // Navigation Component
+    implementation ("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation ("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+}
